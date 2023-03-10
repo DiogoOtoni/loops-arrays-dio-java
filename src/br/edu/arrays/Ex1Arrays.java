aa8a2b78e093f368825575a5c42b3d90a0a593a2
Diogo Otoni <diogootoni@hotmail.com>
2023-03-10 12:12:11 -0300

    Criação e primeiro commit

---
+++ src/br/edu/arrays/Ex1Arrays.java
@@ -0,0 +1,7 @@
+package br.edu.arrays;
+
+public class Ex1Arrays {
+    public static void main(String[] args) {
+        System.out.println("Exercício 1 de Arrays!");
+    }
+}
---
+++ src/br/edu/loops/Ex1Loops.java
@@ -0,0 +1,7 @@
+package br.edu.loops;
+
+public class Ex1Loops {
+    public static void main(String[] args) {
+        System.out.println("Exercícios 1 de Loops.");
+    }
+}
